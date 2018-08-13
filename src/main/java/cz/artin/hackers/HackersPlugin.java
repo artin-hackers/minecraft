7a92c0bb2f90ac055597cfbea3628c2de4c83390
Zdeněk Němec <zdenek.nemec@artin.cz>
2018-08-13 21:57:48 +0200

    Zombie

--- src/main/java/cz/artin/hackers/HackersPlugin.java
+++ src/main/java/cz/artin/hackers/HackersPlugin.java
@@ -1,18 +1,38 @@
 package cz.artin.hackers;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class HackersPlugin extends JavaPlugin {
+    public static final Logger LOG = Logger.getLogger(HackersPlugin.class.getName());
 
     @Override
     public void onEnable() {
-        getLogger().info("Hackers plugin se nacita...");
+        getLogger().info("Loading Hackers plugin...");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("zombie")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                final Location playerLocation = player.getLocation();
+                final Location zombieLocation = new Location(player.getWorld(),
+                        playerLocation.getX() + 5,
+                        playerLocation.getY(),
+                        playerLocation.getZ());
+                Zombie zombie = player.getWorld().spawn(zombieLocation, Zombie.class);
+                player.sendMessage("Zombie near you!");
+                LOG.info("Zombie spawned");
+                return true;
+            }
+        }
 
         return false;
     }
