e22eb3bbe5d8b635c02e092c6b5b7013d811c9ce
Vojtěch Ondráček <xvojta.ondracek@gmail.com>
2018-08-31 17:19:14 +0200

    Move house to his class

--- src/main/java/cz/artin/hackers/HackersPlugin.java
+++ src/main/java/cz/artin/hackers/HackersPlugin.java
@@ -37,135 +37,14 @@ public class HackersPlugin extends JavaPlugin {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
                 final Location playerLocation = player.getLocation();
-                buildHouse(playerLocation);
+                House house = new House(playerLocation);
+                house.buildHouse();
                 return true;
             }
         }
         return false;
     }
 
-    void buildWallX(Location playerLocation) {
-        playerLocation = playerLocation.clone();
-        for (int i = 0; i < 5; i++) {
-            for (int e = 0; e < 5; e++) {
-                playerLocation.add(e, i, 0);
-                playerLocation.getBlock().setType(Material.DARK_OAK_WOOD);
-                playerLocation.add(-e, -i, -0);
-            }
-        }
-    }
-    void buildWallZ(Location playerLocation) {
-        for (int i = 0; i < 5; i++) {
-            for (int e = 0; e < 5; e++) {
-                playerLocation.add(0, i, e);
-                playerLocation.getBlock().setType(Material.DARK_OAK_WOOD);
-                playerLocation.add(-0, -i, -e);
-            }
-        }
-    }
-    void buildRoof(Location playerLocation) {
-        playerLocation = playerLocation.clone();
-
-        playerLocation.add(-1, 4, -1);
-        for (int i = 0; i < 4; i++) {
-            for (int e = 0; e < 4; e++) {
-                playerLocation.add(i, 0, e);
-                playerLocation.getBlock().setType(Material.DARK_OAK_WOOD);
-                playerLocation.add(-i, 0, -e);
-            }
-        }
-
-    }
-    void buildFloor(Location playerLocation) {
-        playerLocation = playerLocation.clone();
-
-        playerLocation.add(-1, -1, -1);
-        for (int i = 0; i < 4; i++) {
-            for (int e = 0; e < 4; e++) {
-                playerLocation.add(i, 0, e);
-                playerLocation.getBlock().setType(Material.DARK_OAK_WOOD);
-                playerLocation.add(-i, 0, -e);
-            }
-        }
-
-    }
-
-    void buildDecorations (Location playerLocation) {
-        playerLocation = playerLocation.clone();
-
-        playerLocation.add(0,0,1);
-        playerLocation.getBlock().setType(Material.CRAFTING_TABLE);
-
-        playerLocation.add(1,0,0);
-        playerLocation.getBlock().setType(Material.ANVIL);
-
-        playerLocation.add(-2,0,0);
-        playerLocation.getBlock().setType(Material.CHEST);
-
-        playerLocation.add(0,0,-2);
-        playerLocation.getBlock().setType(Material.FURNACE);
-
-        playerLocation.add(1,0,0);
-        playerLocation.getBlock().setType(Material.ENCHANTING_TABLE);
-
-        playerLocation.add(1,0,0);
-        playerLocation.getBlock().setType(Material.JUKEBOX);
-
-        playerLocation.add(-1,1,1);
-        playerLocation.add(0,0,2);
-        playerLocation.getBlock().setType(Material.GLASS);
-        playerLocation.add(0,0,-4);
-        playerLocation.getBlock().setType(Material.GLASS);
-    }
-    void buildRoad (Location playerLocation) {
-        playerLocation = playerLocation.clone();
-
-        playerLocation.add(3,0,1);
-        for(int c = 0; c < 6; c++) {
-            playerLocation.add(c, 0, 0);
-            playerLocation.getBlock().setType(Material.POPPY);
-            playerLocation.add(-c,0,0);
-        }
-        playerLocation.add(0,0,-2);
-        for(int c = 0; c < 6; c++) {
-            playerLocation.add(c, 0, 0);
-            playerLocation.getBlock().setType(Material.POPPY);
-            playerLocation.add(-c,0,0);
-        }
-        playerLocation.add(0,-1,1);
-        for(int c = 0; c < 6; c++) {
-            playerLocation.add(c, 0, 0);
-            playerLocation.getBlock().setType(Material.STONE_BRICKS);
-            playerLocation.add(-c,0,0);
-        }
-
-    }
-    void buildDoor (Location playerLocation) {
-        playerLocation = playerLocation.clone();
-
-        playerLocation.add(2, 0, 0);
-        playerLocation.getBlock().setType(Material.AIR);
-        playerLocation.add(0, 1, 0);
-        playerLocation.getBlock().setType(Material.DARK_OAK_TRAPDOOR);
-    }
-
-    void buildHouse (Location playerLocation) {
-        buildFloor(playerLocation);
-        buildRoof(playerLocation);
-        playerLocation.add(-2, -1, 2);
-        buildWallX(playerLocation);
-        playerLocation.add(2, 0, -2);
-        playerLocation.add(-2, 0, -2);
-        buildWallX(playerLocation);
-        buildWallZ(playerLocation);
-        playerLocation.add(2, 0, 2);
-        playerLocation.add(2, 0, -2);
-        buildWallZ(playerLocation);
-        playerLocation.add(-2, 1, 2);
-        buildDoor(playerLocation);
-        buildRoad(playerLocation);
-        buildDecorations(playerLocation);
-    }
 
     void spawnZombie(Player player) {
         final Location playerLocation = player.getLocation();
