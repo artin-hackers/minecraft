52edba6e2e67b2b048dc9691f3cbd455b8d4f9d7
zdenek-nemec <zdenek.nemec@artin.cz>
2018-09-11 12:50:55 +0200

    Prikaz kure

--- src/main/java/cz/artin/hackers/HackersPlugin.java
+++ src/main/java/cz/artin/hackers/HackersPlugin.java
@@ -1,7 +1,10 @@
 package cz.artin.hackers;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Chicken;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HackersPlugin extends JavaPlugin {
@@ -13,7 +16,17 @@ public class HackersPlugin extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("kure")){
+            if(sender instanceof Player){
+                Player player= (Player)sender;
+                Location kureLocation=player.getLocation().clone();
+                kureLocation.add(5,0,0);
+                Chicken kure=player.getWorld().spawn(kureLocation,Chicken.class);
 
+            }
+
+            return true;
+        }
         return false;
     }
 }
